15f3f346971c1d7b6722f27f847c2248cec6f8e7
saadakk <saadakbib53@gmail.com>
2026-05-10 04:45:29 +0100

    add monitoring stack: LGTM + OTel, dockerfiles, observability config

---
+++ Services/GenerateProjectService/src/main/java/ma/ms/generateprojectservice/GenerateProjectServiceApplication.java
@@ -0,0 +1,13 @@
+package ma.ms.generateprojectservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GenerateProjectServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GenerateProjectServiceApplication.class, args);
+	}
+
+}
--- Services/GenerateProjectService/src/test/java/ma/ms/generateprojectservice/GenerateProjectServiceApplicationTests.java
+++ Services/GenerateProjectService/src/test/java/ma/ms/generateprojectservice/GenerateProjectServiceApplicationTests.java
@@ -1,12 +1,13 @@
-package ma.ms.configservice;
+package ma.ms.generateprojectservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
 
-class ConfigServiceApplicationTests {
+@SpringBootTest
+class GenerateProjectServiceApplicationTests {
 
 	@Test
 	void contextLoads() {
 	}
+
 }
